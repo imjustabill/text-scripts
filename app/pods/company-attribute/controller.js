ec8536e5aa8442e3a7301cb1ea50e66e3d2bf23a
Tim Hemmer <themmer@gmail.com>
2016-09-13 17:23:37 -0500

    Text changes, design changes, adding unique types to drop down

--- app/pods/company-attribute/controller.js
+++ app/pods/company-attribute/controller.js
@@ -14,7 +14,7 @@ export default Ember.Controller.extend({
   title: null,
 
   valueType: null,
-  valueTypes: Ember.A(['BIG_DECIMAL', 'BOOLEAN', 'DURATION', 'INTEGER', 'MONEY', 'OBSCURED', 'RESOURCE', 'STRING', 'TIME_ONLY', 'URL']),
+  valueTypes: Ember.A(['BIG_DECIMAL', 'BIG_INTEGER', 'BOOLEAN', 'BYTE', 'DATE', 'DATE_ONLY', 'DATE_TIME', 'DECIMAL', 'DOUBLE', 'DURATION', 'EXPRESSION', 'FLOAT', 'IMAGE_NAME', 'INTEGER', 'LONG', 'MONEY', 'OBSCURED', 'PERCENT', 'RESOURCE', 'SHORT', 'STORED_CONTENT', 'STRING', 'TIME_ONLY', 'URL']),
   availableValuesList: Ember.A(),
   hasAvailableValues: Ember.computed('valueType', {
     get() {
@@ -40,7 +40,8 @@ export default Ember.Controller.extend({
   sectionName: null,
   sectionNames: Ember.A([null, 'Account Opening', 'Accounts', 'ACH', 'Aggregate Other FI Accounts', 'Alerts', 'Authentication', 'Banking Login', 'Bill Pay', 'Biometric Authentication', 'Brokerage Access', 'Budgets', 'Categories', 'Challenge Adapters', 'Check Image', 'Check Orders', 'Client App Information', 'Conduit', 'Conduit System Alerts', 'Core Live', 'Core Live System Alerts', 'D3 Banking', 'D3 Banking Dashboard', 'D3 Control', 'Data Retention', 'Database Alerts', 'Direct Connect', 'E-Bill', 'E-Statements', 'Elasticsearch', 'Encryption', 'Expired User Sessions', 'External Security', 'FAQ', 'Fee Billing', 'First Tennessee SSO', 'Forgot Password', 'General', 'Generic Alerts', 'Goals', 'Hide Accounts', 'iLodo Contract Web Service', 'Inactive User Definition', 'Inactive Users', 'Internal User Alerts', 'LDAP Authentication', 'Locations Search', 'Management Adapters', 'Messages', 'Mobile', 'Mobile Enrollment', 'Money Movement', 'Money Movement Service System Alerts', 'On Us Transfer', 'Open Financial eXchange', 'Out Of Band Verification', 'Overdraft Protection', 'P2P', 'Password', 'Password Reset', 'Past Due Highlighting', 'Pay Multiple', 'Planning', 'Profile', 'RDC', 'Recipients', 'Request Forms', 'Rewards', 'RSA', 'Rules', 'Scheduled Pay', 'Scheduled Transfer', 'Security', 'Self Enrollment', 'Self Service', 'Settings', 'Shadow Assist', 'Snapshot', 'Stop Payment', 'Support Information', 'Synchronization', 'System Alerts', 'Tax Forms', 'Transactions', 'User Account Change', 'User Attributes', 'User Enrollment', 'User History Migration Job', 'User Name', 'User Session', 'User Sync Operations', 'Web Connect']),
 
-  uniqueType: 'NONE',
+  uniqueType: 'null',
+  uniqueTypes: Ember.A(['NONE', 'GLOBAL']),
 
   validationPattern: null,
 
@@ -70,6 +71,10 @@ export default Ember.Controller.extend({
     selectValidationPattern(validationPattern)
     {
       this.set('validationPattern', validationPattern);
+    },
+    selectUniqueType(uniqueType)
+    {
+      this.set('uniqueType', uniqueType);
     }
   }
 
